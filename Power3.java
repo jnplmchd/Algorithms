e1bd8d798ae1862eee1bdb7bc7409092f53f9454
Jean-Paul Michaud <37594523+jnplmchd@users.noreply.github.com>
2019-05-18 17:22:50 -0400

    Update Power3.java

--- Power3.java
+++ Power3.java
@@ -1,30 +1,23 @@
-
+//The idea of this algorithhm is to find base^n using th binary representation of the exponent.
 public class Power3 {
 
-	public static int powerN(int base, int n) {
-		  int binary = 1;
-		  //int count = 0;
-		  while(n>0){
-		    if (n%2 != 0) {
-		    binary = binary *base;
-		    //count = count + 1;
-		   // System.out.println("Count of mult: " + count);
-			
-		    }
-		    n = n/2;
-		    base = base * base;
-		    //count = count + 1;
-		   // System.out.println("Count of mult: " + count);
-			
-		  }
-		  //System.out.println("Count of mult: " + count);
-		  return binary;
-				}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println(powerN(3, 1));
+	public int powerN(int base, int n) {
+	int result = 1;               //the result
+		  
+	int binary = 0;               //the binary representation of each bit of n
+	if(n == 0) {
+         return 1;
 	}
+	  
+	while(n>0){         
+	   binary = n%2;              //is the bit of n 0 or 1?   from right to left
+         if (binary == 1) {
+            result = result *base;  //when bit is 1: result is multiplied by the base
+	  }
+	  n = n/2;                   //move on to the next bit
+	  base = base * base;        //base is squared on every step of the loop
+      }
+     return result;
+}
 
 }
