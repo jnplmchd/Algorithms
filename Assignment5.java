dd2c23d457ab5a70844ab6689c175627d8eb34d8
Jean-Paul Michaud <37594523+jnplmchd@users.noreply.github.com>
2019-05-18 17:12:37 -0400

    Add files via upload

---
+++ Assignment5.java
@@ -0,0 +1,48 @@
+
+public class Assignment5 {
+	
+	//Recursive
+public int[] minMax(int[] nums, int begin, int end) {
+  	
+  	int arr[] = new int[2]; 
+	
+	if (begin == end) {              //case if nums is of size 1
+    arr[0] = nums[begin];
+    arr[1] = nums[end];
+    return arr;
+  }
+  else if (end - 1 == begin) {      //case if nums is of size 2
+    if (nums[begin] < nums[end]) {
+      arr[0] = nums[begin];
+      arr[1] = nums[end];
+    }
+    else {
+      arr[0] = nums[end];
+      arr[1] = nums[begin];
+    }
+     return arr;
+  }
+  else {                           //case if nums is of size > 2
+	  int mid = (end + begin)/2;
+	  int arrLeft[] = minMax(nums, begin, mid);   //left side of nums until it has 2 values
+	  int arrRight[] = minMax(nums, mid+1, end);  //right side of nums untl it has 2 values
+    
+	  //comparing the values of the pairs to find the min and max of the pairs and assign to new array
+    if (arrRight[0] < arrLeft[0]) {
+      arr[0] = arrRight[0];
+    }
+    else {
+      arr[0] = arrLeft[0];
+    }
+    
+    if (arrRight[1] > arrLeft[1]) {
+      arr[0] = arrRight[1];
+    }
+    else {
+      arr[1] = arrLeft[1];
+    }
+    return arr;
+  }
+}
+		} 
+
